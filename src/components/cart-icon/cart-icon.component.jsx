1999cb81bdc9fb699dde436c2a89b6dd0cf64269
Patrick OUAMBO <patgp04@gmail.com>
2022-08-03 17:00:09 +0100

    cart item designs

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -4,7 +4,7 @@ import { CartContext } from "../../context/cart.context";
 import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
 
 function CartIcon() {
-  const { isCartOpen, setIsCartOpen } = useContext(CartContext);
+  const { isCartOpen, setIsCartOpen, cartCount } = useContext(CartContext);
 
   const toggleIsCartOpen = () => {
     setIsCartOpen(!isCartOpen);
@@ -13,7 +13,7 @@ function CartIcon() {
   return (
     <div onClick={toggleIsCartOpen} className="cart-icon-container">
       <ShoppingIcon className="shopping-icon" />
-      <span className="item-count">0</span>
+      <span className="item-count">{cartCount}</span>
     </div>
   );
 }
